880a530a1118ddb31b37feade2034ab6b1ae7773
Mikhail <mikh.krupnov@yandex.ru>
2018-05-08 11:37:25 +0300

    Проверка на флаги

---
+++ mainwindow.cpp
@@ -0,0 +1,53 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "QDir"
+#include "QFile"
+#include "QFileInfo"
+#include "QDebug"
+#include <QtGui>
+#include "QString"
+#include "QFileDialog"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+}
+
+
+MainWindow::~MainWindow()
+{
+    //    qDebug() << directory;
+    //    QFileInfoList list = directory.entryInfoList(static_cast<QDir::Filter>(filters)); // добавить фильтр по подпапкам !!!
+    //    foreach (QFileInfo info, list) {
+    //        qDebug() << info;
+    //        qDebug() << needsomedirects ;
+
+    //        }
+}
+
+
+
+
+void MainWindow::on_addcatalog_clicked()
+{
+
+    QString dialog = QFileDialog::getOpenFileName(this, tr("Open File"));
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    bool needsomedirects =  ui->AllFiles->isChecked();
+    qDebug() << needsomedirects ;
+    QDir directory = QDir("C:\\Users\\user\\Desktop");
+    int filters = 0;
+
+    if (needsomedirects == false) {
+        filters = QDir::Files ;
+    } else {
+        filters = QDir::Files | QDir::AllDirs;
+    }
+    qDebug() << filters;
+}
